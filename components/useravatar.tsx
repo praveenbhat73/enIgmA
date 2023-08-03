3d14aae72ba300d5a02a79c5feac43361c8e3ca0
praveenbhat73 <bhatpraveen721@gmail.com>
2023-08-03 21:41:42 +0530

    added image

--- components/useravatar.tsx
+++ components/useravatar.tsx
@@ -1,13 +1,13 @@
 import { useUser } from "@clerk/nextjs";
-
+// import user1 from "@/public/empty.png"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export const UserAvatar = () => {
   const { user } = useUser();
-
+  const usei=user?.profileImageUrl
   return (
-    <Avatar className="h-8 w-8">
-      <AvatarImage src={user?.profileImageUrl} />
+    <Avatar className="h-8 w-8 bg-white">
+      <AvatarImage src={usei} />
       <AvatarFallback>
         {user?.firstName?.charAt(0)}
         {user?.lastName?.charAt(0)}
